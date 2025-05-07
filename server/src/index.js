3003024a333ff7778e5b30df1b3b6adb2304e76e
Youssef Benbouzid <YOUSSEF.BENBOUZID@elmecspa.it>
2025-05-07 16:42:51 +0200

    Interventi vari

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const utenteRoute = require('./routes/utenteRoute.js')
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 const SECRET_KEY = process.env.SECRET_KEY
 
