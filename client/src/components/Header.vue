3f7fe66218a146d7caa1b93a0ed4f8901bf12d72
Youssef Benbouzid <YOUSSEF.BENBOUZID@elmecspa.it>
2025-05-27 17:57:06 +0200

    Sviluppo interfaccia

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,45 +1,47 @@
 <template>
   <v-app-bar color="#CBCACF">
-    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" />
-
-    <v-toolbar-title>Task List</v-toolbar-title>
-
-    <v-btn icon="mdi-dots-vertical" variant="text" />
-    <v-btn icon="mdi-dots-vertical" variant="text" />
-    <v-btn icon="mdi-dots-vertical" variant="text" />
+    <v-list-item>
+      <img height="40" src="@/assets/logo.svg" width="40">
+    </v-list-item>
+    <v-toolbar-title style="font-family: 'Lucida Handwriting'; font-weight: bold;">Task List</v-toolbar-title>
+    <v-btn v-if="!isLogged" style="background-color: #190BA4; color: #FFFFFF;" class="rounded-xl" text="Accedi" @click="vaiALogin" />
+    <v-btn v-if="!isLogged" style="background-color: #190BA4; color: #FFFFFF;" class="mx-12 rounded-xl" text="Registrati" @click="vaiARegister" />
+    <v-btn v-if="isLogged" style="background-color: #190BA4; color: #FFFFFF;" class="mx-6 rounded-xl" prepend-icon="mdi-plus" text="Aggiungi task" @click="isDialogOpen = true" />
+    <v-list-item v-if="isLogged" class="ml-6" :prepend-avatar="utente.foto" />
   </v-app-bar>
 
-  <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
-    <v-list :items="items" />
+  <v-navigation-drawer elevation="6">
+    <v-list-item v-if="isLogged" prepend-icon="mdi-folder" title="Le mie task" value="mieTask" />
+    <v-divider />
+    <v-list-item v-if="isLogged" prepend-icon="mdi-star" title="Task salvate" value="taskSalvate" />
+    <v-divider />
+    <v-list-item v-if="isLogged" prepend-icon="mdi-account" title="Profilo" value="Profilo" />
+    <v-divider />
+    <v-list-item v-if="isLogged" prepend-icon="mdi-logout" title="Esci" value="logout" @click="logout" />
+    <v-divider />
   </v-navigation-drawer>
 </template>
 
 <script setup>
-  import { ref, watch } from 'vue'
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
 
-  const items = [
-    {
-      title: 'Foo',
-      value: 'foo',
-    },
-    {
-      title: 'Bar',
-      value: 'bar',
-    },
-    {
-      title: 'Fizz',
-      value: 'fizz',
-    },
-    {
-      title: 'Buzz',
-      value: 'buzz',
-    },
-  ]
+  const router = useRouter()
 
-  const drawer = ref(false)
-  const group = ref(null)
+  function vaiALogin() {
+    router.push('/Login')
+  }
 
-  watch(group, () => {
-    drawer.value = false
-  })
+  function vaiARegister() {
+    router.push('/Register')
+  }
+
+  const isLogged = ref(true)
+
+  const utente = {
+    nomeUtente: "Lydia Serena Benbouzid",
+    email: "lydia.serena.benbouzid@email.com",
+    password: "passwordLydia",
+    foto: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+  }
 </script>
