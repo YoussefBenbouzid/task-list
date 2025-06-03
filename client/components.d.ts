53e72ae3956e9ab871eacc7fa7528b4eb9fa73bd
Youssef Benbouzid <YOUSSEF.BENBOUZID@elmecspa.it>
2025-06-03 17:35:19 +0200

    Modifica

--- client/components.d.ts
+++ client/components.d.ts
@@ -9,22 +9,11 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AggiungiTask: typeof import('./src/components/AggiungiTask.vue')['default']
-    Body: typeof import('./src/components/Body.vue')['default']
-    CercaTask: typeof import('./src/components/Body/CercaTask.vue')['default']
-    Footer: typeof import('./src/components/Footer.vue')['default']
     Header: typeof import('./src/components/Header.vue')['default']
-    Home: typeof import('./src/views/Home.vue')['default']
     ListaTask: typeof import('./src/components/ListaTask.vue')['default']
-    Login: typeof import('./src/views/Login.vue')['default']
-    Logout: typeof import('./src/components/Body/Logout.vue')['default']
-    Main: typeof import('./src/components/ListaTask.vue')['default']
-    MieTask: typeof import('./src/components/Body/MieTask.vue')['default']
-    Register: typeof import('./src/views/Register.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SezioneProfilo: typeof import('./src/components/SezioneProfilo.vue')['default']
-    SideBar: typeof import('./src/components/Body/SideBar.vue')['default']
     Task: typeof import('./src/components/Task.vue')['default']
-    TaskSalvate: typeof import('./src/components/Body/TaskSalvate.vue')['default']
   }
 }
